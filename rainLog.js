04011dc1ab130609c52ebeb2e0cbf6fd6be04907
Jake Stephens <sjakedude@gmail.com>
2021-01-15 19:16:54 -0500

    Adding dynamic table

---
+++ rainLog.js
@@ -0,0 +1,10 @@
+new Vue({
+  el: '#app',
+  data: {
+    teamRows: [
+      { team: 'Beach', times: ['0.01', '0.2', '0.16'] },
+      { team: 'Boardwalk', times: ['0.4', '0.25', '0.36'] },
+      { team: 'Park', times: ['0.0', '0.1', '0.0'] }
+    ]
+  }
+});
