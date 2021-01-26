6d1556e77d2275e7d6e75377a8f17ce4d20b08b5
Jared Gonzales <jrg2221@columbia.edu>
2021-01-25 20:04:28 -0700

    Created API weather function
    Replicates functionality of original API but will later be sent to our own Firestore database.

--- functions/index.js
+++ functions/index.js
@@ -7,3 +7,45 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+// Use fetch for HTTP requests
+const fetch = require("node-fetch");
+
+// The Firebase Admin SDK to access Cloud Firestore.
+//const admin = require('firebase-admin');
+
+// Firestore
+//const db = admin.firestore();
+
+
+
+exports.getSiteInfo = functions.https.onRequest(async (request, response) => {
+    const weatherAPIKey = "a77d00cf8cb244f4801195048211101";
+    var date = "2021-01-22";
+    var location = "30.26534,-81.43901";
+    var data = await getWeatherData(weatherAPIKey, location, date);
+    response.send(data);
+});
+
+
+// Grab weather data everyday and store in our own database
+// TODO: Is it necessary to store our data or can we call from the external API everytime?
+function getWeatherData(weatherAPIKey, siteLocation, date) {
+    const requestUrl = "https://api.weatherapi.com/v1/history.json?key=" + weatherAPIKey + "&q=" + siteLocation + "&dt=" + date;
+
+    // Fetch data from requestUrl
+    return fetch(requestUrl).then((response) => {
+        return response.json();
+    }).then((data) => {
+        console.log(data);
+        console.log("Total precip: " + data.forecast.forecastday[0].day.totalprecip_in);
+        return data;
+    }).catch((error) => {
+        console.error("Problem fetching weather: ", error);
+    });
+}
+
+// Add weather data to Firestore database
+function addWeatherData(site, date, hourlyRainfall, totalRainfall) {
+
+}
