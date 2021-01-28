ffe0dcfc116466dc6e2050410da95bea9dfca4d3
Jared Gonzales <jrg2221@columbia.edu>
2021-01-27 18:07:11 -0700

    Front end database retrieval
    Not working entirely but does read user data

--- public/rainLog.js
+++ public/rainLog.js
@@ -8,3 +8,40 @@ new Vue({
     ]
   }
 });
+
+
+var db = firebase.firestore();
+getCurrentMonthWeather();
+
+async function getCurrentMonthWeather() {
+
+  var date = new Date();
+  var month =  ("0" + date.getMonth()).slice(-2);
+  // Date
+  var day = ("0" + date.getDate()).slice(-2);
+
+  var userDropdown = document.getElementById("employees");
+  var user = userDropdown.options[userDropdown.selectedIndex].text.toLowerCase();
+  var sites = await getUserSites(user);
+  console.log(sites[0]);
+  for (site in sites) {
+    var path = sites[site]._path.segments
+    console.log(path);
+    var siteData = db.doc(path).get().then((data) => {
+      return data.data();
+    });
+    var daysMap = siteData["precip_" + month];
+    var todayPrecip = daysMap[day];
+    console.log("Today's precip at " + siteData.name + ": " + todayPrecip);
+  }
+}
+
+function getUserSites(user) {
+  console.log("Getting user data for " + user);
+  var userRef = db.collection("users").doc(user);
+  return userRef.get().then((data) => {
+    var userData = data.data();
+    console.log(userData);
+    return userData.sites;
+  });
+}
