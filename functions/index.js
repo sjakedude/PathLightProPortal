7b24ed5b9ad11a7fc13f07a7abcc0d45eab0e9dc
Jake Stephens <sjakedude@gmail.com>
2021-01-28 22:22:58 -0500

    Add method for updating firestore with weather data

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,5 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
 // Use fetch for HTTP requests
 const fetch = require("node-fetch");
 
@@ -17,46 +9,68 @@ admin.initializeApp();
 
 // Firestore
 const db = admin.firestore();
-
 const cityRef = db.collection('cities').doc('BJ');
 
 
-
+// Main method
 exports.populateWeather = functions.https.onRequest(async (request, response) => {
-    const weatherAPIKey = "a77d00cf8cb244f4801195048211101";
-    var date = "2021-01-22";
 
+    // API key for weather app
+    const weatherAPIKey = "a77d00cf8cb244f4801195048211101";
+
+    // Getting the date into the correct format
+    var dateObject = new Date();
+    var year = dateObject.getFullYear();
+    var month = ("0" + dateObject.getMonth() + 1).slice(-2);
+    var day = (("0" + dateObject.getDate()) - 1).toString().slice(-2);
+    var date = year + "-" + month + "-" + day;
+
+    // Getting all regions (currently only st johns)
     var regions = await getRegions();
+
     for (region in regions) {
+
+        // Get all sites for the region
         var sites = await getSites(region);
-        // Get coordinates from sites/{siteID}
+
+        // Iterate through each site in the region
         for (site in sites) {
+
+            // Grabbing the path to the site in the firestore
             var path = sites[site]._path.segments
-            var coordinates = await getCoordinates(path);
-            console.log("Coordinates for site: " + coordinates)
+            var siteInfo = await getSiteInfo(path);
+
+            // Parsing out site info
+            var siteName = siteInfo.data().name;
+            var latitude = siteInfo.data().coordinates._latitude;
+            var longitude = siteInfo.data().coordinates._longitude;
+            var coordinates = latitude + "," + longitude;
+            console.log("=======\n" + "Site: " + siteName + "\nCoordinates: " + coordinates);
+
             // Calling the weather API for that location
             var data = await getWeatherData(weatherAPIKey, coordinates, date);
-            console.log("++++++++++++++++++")
-            console.log(data)
-            console.log("++++++++++++++++++")
-            // TODO: Call function to populate document with rainfall data
+            console.log("Precipitation: " + data)
+
+            // Updating the firestore with yesterday's precipitation data
+            updateWeatherData(path, data)
         }
-        // Then use getWeatherData() for each site's coordinates
     }
     
+    // If all is good, send response
     response.send("OK");
 });
 
-function getCoordinates(path) {
+function updateWeatherData(path, data) {
+    var siteData = db.doc(path.join("/"));
+    siteData.update({"precip_01.27": data})
+}
+
+function getSiteInfo(path) {
     var siteData = db.doc(path.join("/"));
         return siteData.get().then((data) => {
-            var latitude = data.data().coordinates._latitude;
-            var longitude = data.data().coordinates._longitude;
-            coordinates = latitude + "," + longitude;
-            console.log(coordinates);
-            return coordinates;
+            return data;
         }).catch((err) => {
-            console.error("Failed grabbing coords", err);
+            console.error("Failed grabbing coordinates", err);
         });
 }
 
@@ -75,14 +89,11 @@ function getSites(region) {
     var sitesRef = db.collection("regions").doc("stjohns")
     return sitesRef.get().then((data) => {
         var regionData = data.data();
-        console.log("regionData: " + regionData);
-        console.log("active sites: " + regionData.active_sites)
         return regionData.active_sites;
     }).catch((err) => {
         console.error("Failed to get active sites for " + region + ": ", err)
     });
 }
-
 
 // Grab weather data everyday and store in our own database
 // TODO: Is it necessary to store our data or can we call from the external API everytime?
@@ -93,16 +104,9 @@ function getWeatherData(weatherAPIKey, coordinates, date) {
     return fetch(requestUrl).then((response) => {
         return response.json();
     }).then((data) => {
-        //console.log(data);
         totalPrecip = data.forecast.forecastday[0].day.totalprecip_in
-        //console.log("Total precip: " + data.forecast.forecastday[0].day.totalprecip_in);
         return totalPrecip;
     }).catch((error) => {
-        console.error("Problem fetching weather: ", error);
+        console.error("Problem fetching weather", error);
     });
-}
-
-// Add weather data to Firestore database
-function addWeatherData(site, date, hourlyRainfall, totalRainfall) {
-
 }
