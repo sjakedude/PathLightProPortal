18220e564e35c3eb3125cbc91c4821ae3d8a7e0b
Jake Stephens <sjakedude@gmail.com>
2021-01-31 22:38:19 -0500

    removing close button from modal

--- public/rainLog.js
+++ public/rainLog.js
@@ -11,10 +11,6 @@ new Vue({
 
 // Popup window vars
 var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
-span.onclick = function() {
-  modal.style.display = "none";
-}
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
