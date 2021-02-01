db321026f9b47f40898f306f09c0182e4627e287
Jake Stephens <sjakedude@gmail.com>
2021-01-31 21:49:48 -0500

    add alternative way to escape graph view

--- public/rainLog.js
+++ public/rainLog.js
@@ -15,6 +15,11 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 }
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
 var db = firebase.firestore();
 getCurrentMonthWeather();
